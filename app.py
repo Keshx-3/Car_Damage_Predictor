b961bd11e3dfab21d858da5476e98436ea11f895
Kesavadas <kesavadaskesh@gmail.com>
2025-03-01 15:27:16 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from model_helper import predict
-from PIL import Image
 
 st.title("Vehicle Damage Detection")
 
@@ -10,19 +9,6 @@ if uploaded_file:
     image_path = "temp_file.jpg"
     with open(image_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
-
-    # Open the image using Pillow
-    img = Image.open(image_path)
-    
-    # Resize the image
-    img = img.resize((300, 300))
-    
-    # Save the resized image
-    img.save(image_path)
-
-    # Display the resized image
-    st.image(image_path, caption="Uploaded File", use_container_width=True)
-    
-    # Get prediction
-    prediction = predict(image_path)
-    st.info(f"Predicted Class: {prediction}")
+        st.image(uploaded_file, caption="Uploaded File", use_container_width=True)
+        prediction = predict(image_path)
+        st.info(f"Predicted Class: {prediction}")
